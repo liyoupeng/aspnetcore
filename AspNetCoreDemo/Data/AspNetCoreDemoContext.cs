f783736c6131cb61b17096ccc32f59e532e35877
xhlyp3 <liyoupeng.1984@gmail.com>
2018-11-07 14:10:54 +0800

    创建的项目

---
+++ AspNetCoreDemo/Data/AspNetCoreDemoContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreDemo.Models
+{
+    public class AspNetCoreDemoContext : DbContext
+    {
+        public AspNetCoreDemoContext (DbContextOptions<AspNetCoreDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AspNetCoreDemo.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ AspNetCoreDemo/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreDemo.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+    }
+}
